a0e3e19f2744097d55aed9d1ed05792fc1352eb3
emilianog4 <emiliano.gutierrez4@gmail.com>
2025-10-02 19:57:30 -0300

    agregue activity_olvidaste_contrasena.xml

---
+++ app/src/main/java/com/example/myapplication/OlvidasteContrasenaActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OlvidasteContrasenaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_olvidaste_contrasena)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.olvidaste_contrasena)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
